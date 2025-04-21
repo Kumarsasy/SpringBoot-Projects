288b972d2a9536727385d656da8a54dc79a7af40
Sasikumar <54315941+Kumarsasy@users.noreply.github.com>
2025-04-21 11:37:20 +0530

    Spring Boot Rest Crud

---
+++ 02-Spring-Boot-Rest-Crud/src/main/java/com/cruddemo/dao/EmployeeDAO.java
@@ -0,0 +1,26 @@
+package com.cruddemo.dao;
+
+import com.cruddemo.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+
+    List<Employee> findAll();
+
+    Employee findById(int theId);
+
+    Employee save(Employee theEmployee);
+
+    void deleteById(int theId);
+}
+
+
+
+
+
+
+
+
+
+
